f428a32095050d9814dfe4c0059d7654a2a64aa8
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-06-23 09:34:18 -0500

    Updated docstring. Removed data reshaping/indexing

--- diffpy/snmf/correlation.py
+++ diffpy/snmf/correlation.py
@@ -1,19 +1,23 @@
 import numpy as np
 
 
-def pearson(data):
+def pearson(data1, data2):
     """
+    Returns the pearson correlation coefficient between two data PDF/XRD patterns
 
     Parameters
     ----------
-    data
-
+    data1: ndarray
+        An array of the values of a XRD or PDF pattern
+    data2: ndarray
+        An array of the values of a XRD or PDF pattern
     Returns
     -------
+    float
+        The pearson correlation coefficient between data1 and data2
 
     """
-    data = np.asarray(data)
-    x = data[:, 0]
-    y = data[:, 1]
-    correlation_matrix = np.corrcoef(x, y)
+    data1 = np.asarray(data1)
+    data2 = np.asarray(data2)
+    correlation_matrix = np.corrcoef(data1, data2)
     return correlation_matrix[0, 1]
