8aa103ddec2264113f9c855e992d240c7899c82f
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-06-23 19:32:30 -0500

    work in progress.

--- diffpy/snmf/optimization.py
+++ diffpy/snmf/optimization.py
@@ -1,5 +1,32 @@
 import numpy as np
 import cvxpy as cp
 
-def mkr_box():
-    pass
+
+def mkr_box(Q, q, b):
+    """
+    Solves min J(x) = q'x + (1/2) * x'Qx where x <= b and Q is symmetric positive definite
+
+    Parameters
+    ----------
+    Q
+    q
+    b
+
+    Returns
+    -------
+
+    """
+    Q = np.asarray(Q)
+    q = np.asarray(q)
+    b = np.asarray(b)
+
+    n = Q.shape[0]
+    x = cp.Variable(n)
+
+    objective = cp.Minimize(q.T @ x + 0.5 * cp.quad_form(x, Q))
+    constraints = [x <= b]
+
+    problem = cp.Problem(objective, constraints)
+    problem.solve()
+
+    return x.value
