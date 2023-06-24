a44a110edfeed804b2e9da28b7adf63d2396e243
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-06-24 16:12:52 -0500

    Updated docstring

--- diffpy/snmf/factorizers.py
+++ diffpy/snmf/factorizers.py
@@ -4,18 +4,19 @@ import scipy.optimize
 
 def lsqnonneg(coefficient, target):
     """
+    Solve ``argmin_x || Ax - b ||_2`` for ``x>=0``.
 
     Parameters
     ----------
-    coefficient: ndarray
+    coefficient: ndarray or list of lists
         The coefficient matrix of the least squares problem
-    target: ndarray
+    target: ndarray or list of lists
         The target vector for the least squares problem
 
     Returns
     -------
     ndarray
-        The solution to the least squares problem
+        The solution vector to the least squares problem
     """
     coefficient = np.asarray(coefficient)
     target = np.asarray(target)
