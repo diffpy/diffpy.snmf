3a997e58199586c115c93c373abf8393714bc509
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-06-27 18:33:54 -0500

    Updated docstring, changed to pathlib, work in progress

--- diffpy/snmf/files.py
+++ diffpy/snmf/files.py
@@ -1,28 +1,32 @@
-import os
+from pathlib import Path
 import numpy as np
 
+
 def processing(*path):
-    """
-    Returns a ndarray of PDF/XRD values and a vector of independent variable values corresponding with each output.
+    """Processes a directory of a series of PDF/XRD patterns into a usable format.
+
+    Constructs a 2d array out of a directory of PDF/XRD patterns containing each files dependent variable column in a
+    new column. Constructs a 1d array containing the independent variable values.
 
     Parameters
     ----------
-    path: string
-       An optional string specifying the location of a file containing only PDF/XRD patterns. If empty, the program will
-       look in the current directory.
+    path: str, optional
+      The specific location/path of a directory containing only PDF or XRD data. The default is the path of the
+      current working directory. It may be a POSIX or Windows path.
 
     Returns
     -------
+    tuple
+      The output containing a 2d array containing a PDF/XRD pattern as each of its columns and a 1d array containing the
+      independent variable values of the PDF/XRD pattern series.
 
     """
+    if not path:
+        directory_path = Path.cwd()
+    else:
+        directory_path = Path(path)
 
-    pattern_list = []
-    directory = os.fsencode(*path)
-    for file in os.scandir(directory):
-        pattern_list.append(file)
-
-    for file in pattern_list:
-        with open(fr"{file.name}", 'r') as input_file:
-            for line in input_file:
-                print(line)
-
+    for pattern_path in directory_path.glob('*'):
+        if pattern_path.is_file():
+            with pattern_path.open() as in_file:
+                for line in in_file:
