60c0453aa3ea7dae06b1757b6836173227a56e53
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-06-27 16:24:17 -0500

    updated docstring

--- diffpy/snmf/optimization.py
+++ diffpy/snmf/optimization.py
@@ -2,36 +2,39 @@ import numpy as np
 import cvxpy as cp
 
 
-def mkr_box(quadratic_coefficient, linear_coefficient, lower_bound, upper_bound):
-    """
+def mkr_box(stretched_component_gram_matrix, linear_coefficient, lower_bound, upper_bound):
+    """ Finds the weightings of stretched component signals
+
     Solves min J(x) = (linear_coefficient)' * x + (1/2) * x' * (quadratic coefficient) * x where lower_bound <= x <=
-    upper_bound and quadratic_coefficient is symmetric positive definite
+    upper_bound and stretched_component_gram_matrix is symmetric positive definite
 
     Parameters
     ----------
-    quadratic_coefficient: 2d array like
-        A symmetric positive definite 2 dimensional array like object.
+    stretched_component_gram_matrix: 2d array like
+      The Gram matrix constructed from the stretched component matrix. It is a square positive definite matrix. It has
+      dimensions C x C where C is the number of component signals.
 
     linear_coefficient: 1d array like
-        A vector
+      The vector containing
 
     lower_bound: 1d array like
-        Represents the lower bounds on the value of x element wise
+      The lower limit on the value of the output element wise. Has length C
 
     upper_bound: 1d array like
-        Represents the upper bounds on the value of x element wise.
+      The upper limit on the values of the output element wise. Has length C.
 
     Returns
     -------
     1d array like
+      The vector containing the weightings of the component signal at a certain moment. Has length C.
 
     Raises
     ------
     ValueError
-        If P is not a Hermitian matrix
+        If stretched_component_gram_matrix is not a Symmetric Positive Definite Matrix.
 
     """
-    quadratic_coefficient = np.asarray(quadratic_coefficient)
+    stretched_component_gram_matrix = np.asarray(stretched_component_gram_matrix)
     linear_coefficient = np.asarray(linear_coefficient)
     upper_bound = np.asarray(upper_bound)
     lower_bound = np.asarray(lower_bound)
@@ -40,7 +43,7 @@ def mkr_box(quadratic_coefficient, linear_coefficient, lower_bound, upper_bound)
     solution_variable = cp.Variable(problem_size)
 
     objective = cp.Minimize(
-        linear_coefficient.T @ solution_variable + 0.5 * cp.quad_form(solution_variable, quadratic_coefficient))
+        linear_coefficient.T @ solution_variable + 0.5 * cp.quad_form(solution_variable, stretched_component_gram_matrix))
     constraints = [lower_bound <= solution_variable, solution_variable <= upper_bound]
 
     problem = cp.Problem(objective, constraints)
