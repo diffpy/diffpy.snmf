4c45b001e9d799b8a5ca859e43d2a9d4c5e60ce7
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-07-02 10:57:25 -0500

    added load_input_signals function

--- diffpy/snmf/io.py
+++ diffpy/snmf/io.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.sparse
+from pathlib import Path
 
 
 def initialize_variables(data_input, component_amount, data_type, sparsity=1, smoothness=1e18):
@@ -60,3 +61,43 @@ def initialize_variables(data_input, component_amount, data_type, sparsity=1, sm
         "sparsity_term": sparsity_term
 
     }
+
+def load_input_signals(file_path=None):
+    """Processes a directory of a series of PDF/XRD patterns into a usable format.
+
+    Constructs a 2d array out of a directory of PDF/XRD patterns containing each files dependent variable column in a
+    new column. Constructs a 1d array containing the independent variable values.
+
+    Parameters
+    ----------
+    file_path: str or Path object, optional
+      The path to the directory containing the input data. If no path is specified, defaults to the current working
+      directory.  Accepts a string or a pathlib.Path object.
+
+    Returns
+    -------
+    tuple
+      The output containing a 2d array containing a PDF/XRD pattern as each of its columns and a 1d array containing the
+      independent variable values of the PDF/XRD pattern series.
+
+    """
+
+    if file_path is None:
+        directory_path = Path.cwd()
+    else:
+        directory_path = Path(file_path)
+
+    for pattern_path in directory_path.glob('*'):
+
+        if pattern_path.is_file():
+            with pattern_path.open() as in_file:
+                data_list = [line.strip().split() for line in in_file]
+                values_list = []
+                grid_list = []
+                values_array = np.empty((len(data_list)))
+                grid_points = np.empty((len(data_list)))
+
+                for point in data_list:
+                    pass
+
+
