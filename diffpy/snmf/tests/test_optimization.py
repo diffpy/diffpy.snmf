fa351a1068a07acd0215e7a6b9d4a2cfcc083f61
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-07-05 19:53:22 -0500

    fixed broken tests

--- diffpy/snmf/tests/test_optimization.py
+++ diffpy/snmf/tests/test_optimization.py
@@ -2,7 +2,7 @@ import pytest
 from diffpy.snmf.optimizers import get_weights
 
 tm = [
-    ([[[1, 0], [0, 1]], [1, 1], 0, 1], [0, 0]),
+    ([[[1, 0], [0, 1]], [1, 1], [0, 0], [1, 1]], [0, 0]),
     ([[[1, 0], [0, 1]], [1, 1], -1, 1], [-1, -1]),
     ([[[2, -1, 0], [-1, 2, -1], [0, -1, 2]], [1, 1, 1], -10, 12], [-1.5, -2, -1.5]),
     ([[[2, -1, 0], [-1, 2, -1], [0, -1, 2]], [1, -1, -1], -10, 12], [0, 1, 1]),
@@ -14,4 +14,4 @@ tm = [
 def test_get_weights(tm):
     expected = tm[1]
     actual = get_weights(tm[0][0], tm[0][1], tm[0][2], tm[0][3])
-    assert (actual == pytest.approx(expected))
+    assert (actual == pytest.approx(expected, rel=1e-4, abs=1e-6))
