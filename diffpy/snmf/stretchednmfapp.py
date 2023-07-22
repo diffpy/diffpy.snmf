a024bb5b40ff550963db59aa29e28d4bc8ec1ae5
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-07-22 11:36:56 -0500

    fixed typo out-directory help string

--- diffpy/snmf/stretchednmfapp.py
+++ diffpy/snmf/stretchednmfapp.py
@@ -15,7 +15,7 @@ def create_parser():
     parser.add_argument('-i', '--input-directory', type=str, default=None,
                         help="Directory containing experimental data. Defaults to current working directory.")
     parser.add_argument('-o', '--output-directory', type=str,
-                        help="The directory where the results will be written.  Defaults to '<input_directory>/snmf_results'.")
+                        help="The directory where the results will be written. Defaults to '<input_directory>/snmf_results'.")
     parser.add_argument('-t', '--data-type', type=str, choices=['xrd', 'pdf'],
                         help="The type of the experimental data.")
     parser.add_argument('components', type=int,
