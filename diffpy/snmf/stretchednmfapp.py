0e861486c3a44aac3acbe68c9da520c643cd69cb
Adeolu Ajayi <adeolu.ajayi@protonmail.com>
2023-08-02 13:03:13 -0500

    changed create_parser
    Fixed typo in line 22, renamed 'lift' to 'lift_factor', made ALLOWED_DATA_TYPES global variable

--- diffpy/snmf/stretchednmfapp.py
+++ diffpy/snmf/stretchednmfapp.py
@@ -2,6 +2,8 @@ import numpy as np
 import argparse
 from pathlib import Path
 
+ALLOWED_DATA_TYPES = ['powder_diffraction', 'pd', 'pair_distribution_function', 'pdf']
+
 
 def create_parser():
     parser = argparse.ArgumentParser(
@@ -12,15 +14,16 @@ def create_parser():
                         help="Directory containing experimental data. Defaults to current working directory.")
     parser.add_argument('-o', '--output-directory', type=str,
                         help="The directory where the results will be written. Defaults to '<input_directory/snmf_results>'.")
-    parser.add_argument('t', '--data-type', type=str, choices=['powder_diffraction', 'pdf'],
+    parser.add_argument('t', '--data-type', type=str, choices=ALLOWED_DATA_TYPES,
                         help="The type of the experimental data.")
-    parser.add_argument('-l', '--lift', type=float, default=1,
+    parser.add_argument('-l', '--lift-factor', type=float, default=1,
                         help="The lifting factor. Data will be lifted by lifted_data = data + abs(min(data) * lift). Default is 1.")
-    parser.add_argument('components', type=int,
-                        help="he number of component signals for the NMF decomposition. Must be an integer greater than 0")
+    parser.add_argument('number-of-components', type=int,
+                        help="The number of component signals for the NMF decomposition. Must be an integer greater than 0")
     parser.add_argument('-v', '--version', action='version', help='Print the software version number')
     args = parser.parse_args()
     return args
+
 
 def main():
     pass
