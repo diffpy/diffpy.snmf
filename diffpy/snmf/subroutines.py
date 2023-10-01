f54169535e17bb0832dce714cbb9accb582ea4e5
Adeolu Ajayi <aoa2160@columbia.edu>
2023-09-30 20:36:16 -0400

    Added reconstruct_signal_gra and tests

--- diffpy/snmf/subroutines.py
+++ diffpy/snmf/subroutines.py
@@ -205,7 +205,8 @@ def reconstruct_signal(components, signal_idx):
         stretched_and_weighted = component.apply_weight(signal_idx, stretched)
         reconstruction += stretched_and_weighted
     return reconstruction
-def reconstructed_signal_gra(components, signal_idx):
+
+def reconstruct_signal_gra(components, signal_idx):
     """Reconstruts a specific signal's gradient from its weighted and stretched components.
 
     Calculates the linear combination of stretched components' gradients where each is the strethced component's
@@ -223,7 +224,13 @@ def reconstructed_signal_gra(components, signal_idx):
     1d array like
       The reconstruction of a signal from calculated weights, stretching factors, and iq values.
     """
-
+    signal_length = len(components[0].grid)
+    reconstruction = np.zeros(signal_length)
+    for component in components:
+        stretched = component.apply_stretch(signal_idx)[1]
+        stretched_and_weighted = component.apply_weight(signal_idx, stretched)
+        reconstruction += stretched_and_weighted
+    return reconstruction
 
 
 def initialize_arrays(number_of_components, number_of_moments, signal_length):
