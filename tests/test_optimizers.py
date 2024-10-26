534f62175bf86f1331fb8d219eca6e72d01f3eb6
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-10-26 09:03:27 -0400

    Correclty set the bounds for example set in optimization

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -2,8 +2,9 @@ import pytest
 
 from diffpy.snmf.optimizers import get_weights
 
-tm = [
-    ([[[1, 0], [0, 1]], [1, 1], [0, 0], [1, 1]], [0, 0]),
+test_matrix = [
+    # ([stretched_component_gram_matrix, linear_coefficient, lower_bound, upper_bound], expected)
+    ([[[1, 0], [0, 1]], [1, 1], 0, 0], [0, 0]),
     ([[[1, 0], [0, 1]], [1, 1], -1, 1], [-1, -1]),
     ([[[1.75, 0], [0, 1.5]], [1, 1.2], -1, 1], [-0.571428571428571, -0.8]),
     ([[[0.75, 0.2], [0.2, 0.75]], [-0.1, -0.2], -1, 1], [0.066985645933014, 0.248803827751196]),
@@ -13,8 +14,12 @@ tm = [
 ]
 
 
-@pytest.mark.parametrize("tm", tm)
+@pytest.mark.parametrize("tm", test_matrix)
 def test_get_weights(tm):
+    stretched_component_gram_matrix = tm[0][0]
+    linear_coefficient = tm[0][1]
+    lower_bound = tm[0][2]
+    upper_bound = tm[0][3]
     expected = tm[1]
-    actual = get_weights(tm[0][0], tm[0][1], tm[0][2], tm[0][3])
+    actual = get_weights(stretched_component_gram_matrix, linear_coefficient, lower_bound, upper_bound)
     assert actual == pytest.approx(expected, rel=1e-4, abs=1e-6)
