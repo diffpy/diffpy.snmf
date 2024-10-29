81c402e5d91444837a0ce957097102d51943f5d3
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-10-29 10:30:22 -0400

    Refactor test polynomial

--- tests/test_polynomials.py
+++ tests/test_polynomials.py
@@ -5,24 +5,23 @@ from diffpy.snmf.polynomials import compute_root
 
 
 @pytest.mark.parametrize(
-    "inputs, expected",
+    "linear_coefficient, constant, expected",
     [
-        ([0, 0], 0),
-        ([-99.99, 12.50], 9.936397678254531),
-        ([-4, 0], 2),
-        ([10, 0], 0),
-        ([-7, -7], 3.04891734),
-        ([100, 72], 0),
-        ([1, 3], 0),
-        ([0, -7], 0),
-        ([-9, 0], 3),
-        ([-9, 3], 2.8169),
-        ([[2, 2], 2], [0, 0]),
-        ([[[2, 2], [2, 2]], 2], [[0, 0], [0, 0]]),
-        ([[[[3, 2], [-2, -2], [100, 0]]], 2], [[[0, 0], [0, 0], [0, 0]]]),
+        (0, 0, 0),
+        (-99.99, 12.50, 9.936397678254531),
+        (-4, 0, 2),
+        (10, 0, 0),
+        (-7, -7, 3.04891734),
+        (100, 72, 0),
+        (1, 3, 0),
+        (0, -7, 0),
+        (-9, 0, 3),
+        (-9, 3, 2.8169),
+        ([2, 2], 2, [0, 0]),
+        ([[2, 2], [2, 2]], 2, [[0, 0], [0, 0]]),
+        ([[[3, 2], [-2, -2], [100, 0]]], 2, [[[0, 0], [0, 0], [0, 0]]]),
     ],
 )
-def test_rooth(inputs, expected):
-    linear_coefficient, constants = inputs
-    actual = compute_root(linear_coefficient, constants)
+def test_rooth(linear_coefficient, constant, expected):
+    actual = compute_root(linear_coefficient, constant)
     np.testing.assert_allclose(actual, expected, rtol=1e-4)
