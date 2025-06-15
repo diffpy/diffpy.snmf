decb8d6080f3d762900db211e35af3adb3c3355f
Zhi Ming Xu <zhimingx27@gmail.com>
2025-06-14 20:29:54 -0400

    skpkg: standardize files by removing examples and adding back necessary scripts

--- src/diffpy/snmf/__init__.py
+++ src/diffpy/snmf/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# (c) 2025 The Trustees of Columbia University in the City of New York.
+# (c) 2024-2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
-# File coded by: Simon J.L. Billinge group.
+# File coded by: Billinge Group members and community contributors.
 #
 # See GitHub contributions for a more detailed list of contributors.
 # https://github.com/diffpy/diffpy.snmf/graphs/contributors
