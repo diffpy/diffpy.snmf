4e13df11020b65fb75d8f71ad9a99890fb4f9247
Simon Billinge <sbillinge@users.noreply.github.com>
2025-06-15 17:10:56 -0400

    fix: correct class instantiation after change in import

--- src/diffpy/snmf/main.py
+++ src/diffpy/snmf/main.py
@@ -7,7 +7,7 @@ A0 = np.loadtxt("input/A0.txt", dtype=float)
 Y0 = np.loadtxt("input/W0.txt", dtype=float)
 N, M = MM.shape
 
-my_model = snmf_class.SNMFOptimizer(MM=MM, Y0=Y0, X0=X0, A0=A0)
+my_model = SNMFOptimizer(MM=MM, Y0=Y0, X0=X0, A0=A0)
 print("Done")
 np.savetxt("my_norm_X.txt", my_model.X, fmt="%.6g", delimiter=" ")
 np.savetxt("my_norm_Y.txt", my_model.Y, fmt="%.6g", delimiter=" ")
