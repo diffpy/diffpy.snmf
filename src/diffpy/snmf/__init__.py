cf012b57f79bbafdc44f14245566549f9bf7375e
Zhi Ming Xu <zhimingx27@gmail.com>
2025-06-14 20:02:42 -0400

    skpkg: migrate src folder

--- src/diffpy/snmf/__init__.py
+++ src/diffpy/snmf/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# (c) 2024 The Trustees of Columbia University in the City of New York.
+# (c) 2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
-# File coded by: Billinge Group members and community contributors.
+# File coded by: Simon J.L. Billinge group.
 #
 # See GitHub contributions for a more detailed list of contributors.
 # https://github.com/diffpy/diffpy.snmf/graphs/contributors
@@ -12,11 +12,10 @@
 # See LICENSE.rst for license information.
 #
 ##############################################################################
-
-"""A python package implementing the stretched NMF algorithm."""
+"""Python package implementing the stretched NMF algorithm."""
 
 # package version
-from diffpy.snmf.version import __version__
+from diffpy.snmf.version import __version__  # noqa
 
 # silence the pyflakes syntax checker
 assert __version__ or True
