d3b9b8b1f5c16063db3a9f2a0acc534e0a6e2365
John Halloran <jhalloran@oxy.edu>
2025-06-18 14:45:39 -0700

    Chore: preliminary lowercasing of variables

--- src/diffpy/snmf/main.py
+++ src/diffpy/snmf/main.py
@@ -7,8 +7,8 @@ A0 = np.loadtxt("input/A0.txt", dtype=float)
 Y0 = np.loadtxt("input/W0.txt", dtype=float)
 N, M = MM.shape
 
-my_model = SNMFOptimizer(MM=MM, Y0=Y0, X0=X0, A0=A0)
+my_model = SNMFOptimizer(source_matrix=MM, init_weights=Y0, init_comps=X0, init_stretch=A0)
 print("Done")
-np.savetxt("my_norm_X.txt", my_model.X, fmt="%.6g", delimiter=" ")
-np.savetxt("my_norm_Y.txt", my_model.Y, fmt="%.6g", delimiter=" ")
-np.savetxt("my_norm_A.txt", my_model.A, fmt="%.6g", delimiter=" ")
+np.savetxt("my_norm_X.txt", my_model.comps, fmt="%.6g", delimiter=" ")
+np.savetxt("my_norm_Y.txt", my_model.weights, fmt="%.6g", delimiter=" ")
+np.savetxt("my_norm_A.txt", my_model.stretch, fmt="%.6g", delimiter=" ")
